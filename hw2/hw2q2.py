345dfb67cfee6b35d84b8152868494b3be0d88b5
averyk22 <avery.kuo@gmail.com>
2024-09-12 23:41:38 -0400

    q2

---
+++ hw2/hw2q2.py
@@ -0,0 +1,42 @@
+# Build a k-mer index of a genome for a given value of k from a FASTA file
+# Input: FASTA file and k.txt which contains a single positive integer value
+# Output: two values separated by space to the output file: total # of distinct keys and the number of keys that appear once exactly 
+import sys
+import collections
+from collections import defaultdict
+if len(sys.argv) != 4:
+    print("Usage: python3 hw2q2.py <input_file> <input_file> <output_file>")
+    sys.exit(1)
+
+# Assign the input and output filenames from command-line arguments
+fasta_file = sys.argv[1] # First argument: input filename
+k_file = sys.argv[2] # Second argument: output filename
+output_file = sys.argv[3]
+with open(fasta_file, 'r') as fasta_file:
+    fasta_input = ''
+    for line in fasta_file:
+        for c in line:
+            if c in ('ACGT'):
+                fasta_input += c
+with open(k_file, 'r') as k_file:
+    k_index = k_file.readline().strip()
+    k_index = int(k_index)
+
+# Code taken from k-mer hashmap implementation from class notes
+index = {}
+for i in range(len(fasta_input) - k_index + 1):  # for each k-mer
+    kmer = fasta_input[i:i+k_index]
+    if kmer not in index:
+        index[kmer] = [i]
+    else:
+        index[kmer].append(i)
+# Counting how many keys appear exactly once
+exactly_one = 0
+for value in index.values():
+    if len(value) == 1:
+        exactly_one += 1
+# Counting total number of keys
+total_keys = len(index.keys())
+with open(output_file, 'w') as out_file:
+    out_file.write(f"{total_keys} {exactly_one}")
+
